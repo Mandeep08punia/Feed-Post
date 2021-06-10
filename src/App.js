deb760c7711857dbb30ec9b62dfa689ab6a54909
Mandeep Punia <48849295+Mandeep08punia@users.noreply.github.com>
2021-06-10 15:49:45 +0530

    Post component+ App_header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-
+import React from 'react';
 import './App.css';
+import image from './assets/images/C.png'
+import Post from './component/Post';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      {/*Header */}
+      <div className="app_header">
+        <img src={image} alt="logo" class="app_headerImage" id="logo"/>
+      </div>
+      <Post/>
+
+      {/* Posts*/}
+      {/* */}
     </div>
   );
 }
---
+++ src/component/Post.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+        <h3>Username</h3>
+            {/*header => avater and username */}
+            {/* video */}
+            {/* username and caption */}
+        </div>
+    )
+}
+
+export default Post
