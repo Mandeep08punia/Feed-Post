a4bcdf42d2dd4c2a601a9148b3bda87960c1309f
Mandeep Punia <48849295+Mandeep08punia@users.noreply.github.com>
2021-06-10 17:34:07 +0530

    setup post state

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import image from './assets/images/C.png'
 import Post from './component/Post';
+import video1 from './assets/videos/Struggler.mp4'
 
 function App() {
+  const [posts,setPosts]=useState([
+    {
+      username:"Mandeep Punia", 
+      caption:"Wow", 
+      videoUrl:`${video1}`
+    },
+    {
+      username:"Mandeep Punia", 
+      caption:"Wow", 
+      videoUrl:`${video1}`
+    }
+    
+  ]);
+
   return (
     <div className="app">
       {/*Header */}
       <div className="app_header">
         <img src={image} alt="logo" class="app_headerImage" id="logo"/>
       </div>
-      <Post/>
 
-      {/* Posts*/}
-      {/* */}
+    {
+      posts.map(post=>(
+        <Post username={post.username} caption={post.caption} videoUrl={post.videoUrl}/>
+      ))
+    }
+      
     </div>
   );
 }
